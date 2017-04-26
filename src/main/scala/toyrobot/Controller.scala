cef2938e340b883b7a2bb2f23bb09f7b2e80ac3c
Julian Chan <julian.chan.jk@gmail.com>
2017-04-26 21:54:48 +1000

    added Controller.place method and test

--- src/main/scala/toyrobot/Controller.scala
+++ src/main/scala/toyrobot/Controller.scala
@@ -1,6 +1,8 @@
 package toyrobot
 
 object Controller {
+  def place(x: Int, y: Int, direction: Direction): Robot = Robot(x, y, direction)
+
   def move(robot: Robot): Robot = robot.direction match {
     case North => robot.copy(y = robot.y + 1)
     case East => robot.copy(x = robot.x + 1)
